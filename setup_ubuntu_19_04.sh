812c7fc39305bdc8875ebdd32d6bc49e773ed9cd
dragon585786 <salim.khan@g10consultancy.com>
2019-11-03 15:18:43 +0530

    version 2

--- setup_ubuntu_19_04.sh
+++ setup_ubuntu_19_04.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-echo "  
-   _____      __                             __  ____                __       
-  / ___/___  / /_            __  ______     / / / / /_  __  ______  / /___  __
-  \__ \/ _ \/ __/  ______   / / / / __ \   / / / / __ \/ / / / __ \/ __/ / / /
- ___/ /  __/ /_   /_____/  / /_/ / /_/ /  / /_/ / /_/ / /_/ / / / / /_/ /_/ / 
-/____/\___/\__/            \__,_/ .___/   \____/_.___/\__,_/_/ /_/\__/\__,_/  
-                               /_/
+
+echo "   _____      __                             __  ____                __ "
+sleep 0.5s 
+echo "  / ___/___  / /_            __  ______     / / / / /_  __  ______  / /___  __ "
+sleep 0.5s
+echo "  \__ \/ _ \/ __/  ______   / / / / __ \   / / / / __ \/ / / / __ \/ __/ / / / "
+sleep 0.5s
+echo " ___/ /  __/ /_   /_____/  / /_/ / /_/ /  / /_/ / /_/ / /_/ / / / / /_/ /_/ / "
+sleep 0.5s
+echo "/____/\___/\__/            \__,_/ .___/   \____/_.___/\__,_/_/ /_/\__/\__,_/ "
+sleep 0.5s 
+echo "                               /_/ "
+sleep 0.5s
 
 
 
 
-"	
+
 
 
 while [ "$choice" != 3 ]
@@ -44,24 +50,98 @@ do
                         1)
                             echo "\n\nAny Desk"
                             gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo dpkg -i anydesk_5.1.2-1_amd64.deb'
+                            echo "any desk installing............. "
+                            sleep 10s
+                            
                             ;;
                         2)
                             echo "\n\nDiscord"
+                            # gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt update && sudo apt upgrade -y && sudo dpkg -i discord-0.0.9.deb && sudo apt install -f'
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt update'
+                            echo "sudo apt update............. "
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt upgrade -y'
+                            echo "sudo apt upgrade -y............. "
+                            sleep 20s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo dpkg -i discord-0.0.9.deb'
+                            echo "installing ............. "
+                            sleep 20s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt install -f'
+                            echo "sudo apt update............. "
+             
+                            sleep 10s
+                            
                             ;;
                         3)
                             echo "\n\nSpotify"
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo snap install spotify '
+                            echo "installing spotify ........."
+                            sleep 20s
+
                             ;;
                         4)
                             echo "\n\nTor Browser"
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt update'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt install apt-transport-https'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo add-apt-repository universe'
+                            echo "installing........."                            
+                            sleep 10s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'wget -q -O - https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -'
+                            echo "installing........."
+                            sleep 60s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list'
+                            echo "installing........."
+                            sleep 3s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt update'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt install tor deb.torproject.org-keyring torbrowser-launcher'                       
+                            echo "installing........."
+                            sleep 10s
+
                             ;;
                         5)
                             echo "\n\nVisual Studio Code"
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt update'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt install software-properties-common apt-transport-https wget'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt update'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt install code'
+                            echo "installing........."
+                            sleep 30s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt update'
+                            echo "installing........."
+                            sleep 15s
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo apt upgrade'
+                            echo "installing........."
+                            sleep 15s
+                        #    6 9 20 
                             ;;
                         6)
                             echo "\n\nVlc Media"
+                            gnome-terminal --working-directory=/home/dragon/Desktop/setupUbuntu/ -e 'sudo snap install vlc'
+                            echo "installing........."
+                            sleep 60s
                             ;;
                         7)
                             echo "\n\nBack to Main Menu"
+                            sleep 1s
+
                             break
                             ;;
                         *)
@@ -71,7 +151,15 @@ do
 
                 ;;
         2)
-            echo "ywo is pre..."
+            echo "
+            to download this project
+            git clone https://github.com/dragon585786/setupUbuntu
+            my youtube channel
+            https://www.youtube.com/channel/UCSfyuscg9MpnXaLRw7qgfzw
+            this is a free code you can modify this code as per your requirement 
+                                THE KEY TO SUCCESS 
+                                  IS TO FOCUS ON 
+                                GOALS, NOT OBSTACLES."
                 ;;
         3)
             echo "Thank You For Using This Awesome Script " 
